e7fff5b57f86f5ee80bccff6958a22a408754f81
Chahatyadav1 <chahat200@icloud.com>
2026-03-19 19:25:03 +0530

    fix: all tests passing - chai-http v4, chai.should(), xunit reporter for CI

--- app-test.js
+++ app-test.js
@@ -1,141 +1,131 @@
 let mongoose = require("mongoose");
 let server = require("./app");
-let { expect } = require("chai");
+let chai = require("chai");
 let chaiHttp = require("chai-http");
 
-// chai-http v5: use chaiHttp.request.execute directly, no chai.use() needed
-let request = chaiHttp.request.execute;
+chai.use(chaiHttp);
+chai.should(); // enables res.should.have.status() on all objects
 
 describe('Countries API Suite', () => {
 
     describe('Fetching Country Details', () => {
 
         it('it should fetch a country named India', (done) => {
-            let payload = { id: 1 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 1 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(1);
-                    expect(res.body).to.have.property('name').eql('India');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(1);
+                    res.body.should.have.property('name').eql('India');
                     done();
                 });
         });
 
         it('it should fetch a country named United States', (done) => {
-            let payload = { id: 2 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 2 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(2);
-                    expect(res.body).to.have.property('name').eql('United States');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(2);
+                    res.body.should.have.property('name').eql('United States');
                     done();
                 });
         });
 
         it('it should fetch a country named Germany', (done) => {
-            let payload = { id: 3 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 3 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(3);
-                    expect(res.body).to.have.property('name').eql('Germany');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(3);
+                    res.body.should.have.property('name').eql('Germany');
                     done();
                 });
         });
 
         it('it should fetch a country named Brazil', (done) => {
-            let payload = { id: 4 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 4 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(4);
-                    expect(res.body).to.have.property('name').eql('Brazil');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(4);
+                    res.body.should.have.property('name').eql('Brazil');
                     done();
                 });
         });
 
         it('it should fetch a country named Japan', (done) => {
-            let payload = { id: 5 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 5 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(5);
-                    expect(res.body).to.have.property('name').eql('Japan');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(5);
+                    res.body.should.have.property('name').eql('Japan');
                     done();
                 });
         });
 
         it('it should fetch a country named Australia', (done) => {
-            let payload = { id: 6 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 6 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(6);
-                    expect(res.body).to.have.property('name').eql('Australia');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(6);
+                    res.body.should.have.property('name').eql('Australia');
                     done();
                 });
         });
 
         it('it should fetch a country named South Africa', (done) => {
-            let payload = { id: 7 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 7 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(7);
-                    expect(res.body).to.have.property('name').eql('South Africa');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(7);
+                    res.body.should.have.property('name').eql('South Africa');
                     done();
                 });
         });
 
         it('it should fetch a country named Canada', (done) => {
-            let payload = { id: 8 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 8 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(8);
-                    expect(res.body).to.have.property('name').eql('Canada');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(8);
+                    res.body.should.have.property('name').eql('Canada');
                     done();
                 });
         });
 
         it('it should fetch a country named France', (done) => {
-            let payload = { id: 9 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 9 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(9);
-                    expect(res.body).to.have.property('name').eql('France');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(9);
+                    res.body.should.have.property('name').eql('France');
                     done();
                 });
         });
 
         it('it should fetch a country named Argentina', (done) => {
-            let payload = { id: 10 }
-            request(server)
+            chai.request(server)
                 .post('/country')
-                .send(payload)
+                .send({ id: 10 })
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('id').eql(10);
-                    expect(res.body).to.have.property('name').eql('Argentina');
+                    res.should.have.status(200);
+                    res.body.should.have.property('id').eql(10);
+                    res.body.should.have.property('name').eql('Argentina');
                     done();
                 });
         });
@@ -144,15 +134,14 @@ describe('Countries API Suite', () => {
 
 });
 
-// Use below test cases to achieve coverage
 describe('Testing Other Endpoints', () => {
 
     describe('it should fetch OS Details', () => {
         it('it should fetch OS details', (done) => {
-            request(server)
+            chai.request(server)
                 .get('/os')
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
+                    res.should.have.status(200);
                     done();
                 });
         });
@@ -160,11 +149,11 @@ describe('Testing Other Endpoints', () => {
 
     describe('it should fetch Live Status', () => {
         it('it checks Liveness endpoint', (done) => {
-            request(server)
+            chai.request(server)
                 .get('/live')
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('status').eql('live');
+                    res.should.have.status(200);
+                    res.body.should.have.property('status').eql('live');
                     done();
                 });
         });
@@ -172,11 +161,11 @@ describe('Testing Other Endpoints', () => {
 
     describe('it should fetch Ready Status', () => {
         it('it checks Readiness endpoint', (done) => {
-            request(server)
+            chai.request(server)
                 .get('/ready')
                 .end((err, res) => {
-                    expect(res).to.have.status(200);
-                    expect(res.body).to.have.property('status').eql('ready');
+                    res.should.have.status(200);
+                    res.body.should.have.property('status').eql('ready');
                     done();
                 });
         });
